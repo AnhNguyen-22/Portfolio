c0ff43a8d1244a67096164aa0b51f8f64eb9682f
AnhNguyen-22 <anhnth23416@st.uel.edu.vn>
2026-04-28 03:01:51 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/Portfolio/',
+})
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
